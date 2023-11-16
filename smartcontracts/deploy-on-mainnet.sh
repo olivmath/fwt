69c11137506b9b9ae68d0b33a4bf5e6be1928f5c
Lucas Oliveira <50037567+olivmath@users.noreply.github.com>
2023-11-16 08:19:40 -0300

    Update deploy-on-mainnet.sh

--- smartcontracts/deploy-on-mainnet.sh
+++ smartcontracts/deploy-on-mainnet.sh
@@ -1,6 +1,6 @@
 source .env
 
-forge script scripts/deploy.dao.mainnet.sol:Polygon \
+forge script scripts/deploy.mainnet.s.sol:Polygon \
     --private-key $POLYGON_PRIVATE_KEY \
     --rpc-url $POLYGON_RPC_URL \
     --broadcast
