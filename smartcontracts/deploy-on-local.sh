d907dbb3d700b70757e0f35a416898729a6e3648
Lucas Oliveira <50037567+olivmath@users.noreply.github.com>
2023-11-16 08:22:15 -0300

    Update deploy-on-local.sh

--- smartcontracts/deploy-on-local.sh
+++ smartcontracts/deploy-on-local.sh
@@ -1,6 +1,6 @@
 forge b --skip test script --build-info
 
-forge script script/deploy.s.sol:Local \
+forge script script/deploy.local.s.sol:Local \
     --rpc-url http://127.0.0.1:8545 \
     --build-info \
     --broadcast \
